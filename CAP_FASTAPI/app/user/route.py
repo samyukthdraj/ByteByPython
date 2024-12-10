399dabaa99f2d99b27c9852beaf3bc628e4e7d79
Surya <suryad@terawe.com>
2024-12-10 12:31:18 +0530

    FastApi structured_1

--- CAP_FASTAPI/app/user/route.py
+++ CAP_FASTAPI/app/user/route.py
@@ -5,18 +5,18 @@ from .service import post_user, get_user_by_username, get_all_users
 
 user_router = APIRouter()
 
-@user_router.post("/users/", response_model=User)
+@user_router.post("/post/users", response_model=User)
 def post_user_route(user: User):
     return post_user(user)
 
-@user_router.get("/users/{user_name}", response_model=User)
+@user_router.get("/get/userByUserName/{user_name}", response_model=User)
 def get_user_route(user_name: str):
     user = get_user_by_username(user_name)
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return user
 
-@user_router.get("/users", response_model=List[User])
+@user_router.get("/get/users", response_model=List[User])
 def get_all_users_route():
     users = get_all_users()
     if not users:
