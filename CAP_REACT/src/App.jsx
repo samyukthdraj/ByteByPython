eb077ebe629daeda260fc89f3d068e34f0096890
Ashwin Linu <ashwinl@terawe.com>
2024-12-11 14:58:34 +0530

    my latest changes with token backend

--- CAP_REACT/src/App.jsx
+++ CAP_REACT/src/App.jsx
@@ -5,16 +5,7 @@ import SignUp from './components/pages/signUp'
 import NewToken from './components/pages/newToken'
 import ForgotPassword from './components/pages/ForgotPassword'
 
-// const router = createBrowserRouter([
-//   {
-//     path:'/',
-//     children:[
-//       {path: '/',element:<SignIn/>},
-//       {path: '/signUp',element:<SignUp/>},
-//       {path: '/newToken',element:<NewToken/>}
-//     ]
-//   }
-// ])
+
 
 const router = createBrowserRouter([
   {
@@ -36,35 +27,10 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  // return <RouterProvider router={router} />;
-  < NewToken />
+  return <NewToken />;
+  // < NewToken />
 }
 
 export default App
-// import './App.css';
-// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-// import SignIn from './components/pages/signIn';
-// import SignUp from './components/pages/signUp';
-// import NewToken from './components/pages/newToken';
 
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <SignIn />, // Root route goes to SignIn component
-//   },
-//   {
-//     path: '/signUp',
-//     element: <SignUp />, // Route for SignUp page
-//   },
-//   {
-//     path: '/newToken',
-//     element: <NewToken />, // Route for NewToken (CAP Dashboard)
-//   },
-// ]);
-
-// function App() {
-//   return <RouterProvider router={router} />;
-// }
-
-// export default App;
 
